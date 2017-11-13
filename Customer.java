6899b7818959e9d4bb501a84c0e3b365aa3012d8
yj.hou <houmanager@qq.com>
2017-11-13 15:54:10 +0800

    :fire: update file

---
+++ Customer.java
@@ -0,0 +1,15 @@
+package cn.itcast.crm.entity;
+
+public class Customer {
+	
+	private Long custId;
+	private String custName;
+	private Long custUserId;
+	private Long custCreateId;
+	private String custIndustry;
+	private String custLevel;
+	private String custLinkman;
+	private String custPhone;
+	private String custMobile;
+	
+}
